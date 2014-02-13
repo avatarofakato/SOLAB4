3c95294bbacfffae0fe3c448bd61383264e09b84
Adam Baranowski <adam.baranowski1@gazeta.pl>
2014-02-13 21:11:55 +0100

    beginings of wsclock

--- pagesim.c
+++ pagesim.c
@@ -1,6 +1,9 @@
 #include "pagesim.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+
+pthread_mutex_t mutex;
 
 // TODO do we need enum name?
 enum {
@@ -14,10 +17,22 @@ typedef enum {
 	ND
 } position;
 
+typedef struct {
+	int bylo_odwolanie;
+	int czas_odwolania;
+	int modyfikowana;
+	int trwa_transmisja;
+	int proces;
+	int nastepna_wolna;
+} frame;
+
 int debug = 1;
 
 int first_time_init = 1;
-int first_time_end = 1;
+int end = 0;
+
+unsigned number_of_pagesim_call = 0;
+const unsigned T = 3;
 
 int *virtual_memory;
 
@@ -32,29 +47,146 @@ unsigned strategy;
 unsigned max_concurrent_operations;
 /* pagesim_callback *callback; */
 
-// Simulates 2D arrays
+int strzalka;
+int wolna;
+int usuwane;
+int czekajacy;
+int biezacy_proces;
+
+frame *PaO;
+
+// TODO
+/* (* inicjuje przesłanie strony z danej ramki z pamięci operacyjnej do pomocniczej *) */
+void UsunStrone(int nrramki)
+{
+}
+
+// TODO
+/* (* wstrzymuje działanie procesu aż do chwili najbliższego zakończenia usuwania */
+int CzekajNaRamke()
+{
+	return 0;
+}
+
+// TODO
+/* (* przekazuje numer procesu wyznaczonego do usunięcia *) */
+int UsunProces()
+{
+	return 0;
+}
+
+void ZwolnijRamke(int nrramki)
+{
+	PaO[nrramki].nastepna_wolna = wolna;
+	wolna = nrramki;
+
+	PaO[nrramki].proces = 0;
+	PaO[nrramki].trwa_transmisja = 0;
+	PaO[nrramki].bylo_odwolanie = 0;
+}
+
+void WSClock(int *nrramki)
+{
+	int *znaleziono;
+	int ostatni;
+	int nrproc;
+
+	if(wolna != -1)
+	{
+		*nrramki = wolna;
+		wolna = PaO[wolna].nastepna_wolna;
+	}
+	else
+	{
+		*znaleziono = 0;
+		ostatni = strzalka;
+		while(znaleziono || (strzalka == ostatni))
+		{
+			strzalka = (strzalka + 1) % mem_size;
+			if(!PaO[strzalka].trwa_transmisja)
+				if(PaO[strzalka].bylo_odwolanie)
+				{
+					PaO[strzalka].bylo_odwolanie = 0;
+					PaO[strzalka].czas_odwolania = number_of_pagesim_call;
+				}
+				else
+					if(abs(number_of_pagesim_call - PaO[strzalka].czas_odwolania) > T)
+						if(PaO[strzalka].modyfikowana)
+						{
+							usuwane++;
+							PaO[strzalka].trwa_transmisja = 1;
+							UsunStrone(strzalka);
+						}
+						else
+						{
+							*znaleziono = 1;
+							PaO[strzalka].proces = biezacy_proces;
+							*nrramki = strzalka;
+						}
+		}
+
+		if(!znaleziono)
+		{
+			if(czekajacy >= usuwane)
+			{
+				nrproc = UsunProces();
+			}
+
+			size_t ostatni;
+			for(ostatni = 0; ostatni < mem_size; ostatni++)
+			{
+				if(PaO[ostatni].proces == nrproc)
+				{
+					PaO[ostatni].trwa_transmisja = 1;
+					usuwane++;
+					UsunStrone(ostatni);
+				}
+				else
+					ZwolnijRamke(ostatni);
+			}
+			
+			if(wolna != -1)
+			{
+				*nrramki = wolna;
+				wolna = PaO[wolna].nastepna_wolna;
+			}
+			else
+			{
+				czekajacy++;
+				*nrramki = CzekajNaRamke();
+			}
+		}
+	}
+}
+
+// Simulates 2D arrays TODO is mutex needed?
 int get_frame(int index, position pos)
 {
+	pthread_mutex_lock(&mutex);
 	return frames[index + pos * mem_size];
+	pthread_mutex_unlock(&mutex);
 }
 
 void set_frame(int index, position pos, int value)
 {
+	pthread_mutex_lock(&mutex);
 	frames[index + pos * mem_size] = value;
+	pthread_mutex_unlock(&mutex);
 }
 
+// If page is already loaded into some frame, returns its index otherwise -1
 int is_page_available(int page_number)
 {
-	int ret = -1;
 	size_t i;
 	for(i = 0; i < mem_size; i++)
+	{
+		pthread_mutex_lock(&mutex);
 		if(get_frame(i, ST) == page_number)
-		{
-			ret = i;
-			break;
-		}
+			return i;
+		pthread_mutex_unlock(&mutex);
+	}
 
-	return ret;
+	return -1;
 }
 
 int page_sim_init(unsigned _page_size, unsigned _mem_size,
@@ -63,14 +195,19 @@ int page_sim_init(unsigned _page_size, unsigned _mem_size,
 				  pagesim_callback _callback)
 {
 	if(debug)
-		printf("page_sim_init(%u, %u, %u, %u, %u, callback) called \n", page_size, mem_size, addr_space_size, strategy, max_concurrent_operations);
+		printf("page_sim_init(%u, %u, %u, %u, %u, callback) called \n",
+	_page_size, _mem_size, _addr_space_size, _strategy,
+	_max_concurrent_operations);
 
 	// TODO errno
-	if(!first_time_init)
+	pthread_mutex_lock(&mutex);
+	if(!first_time_init || end)
+	{
+		pthread_mutex_unlock(&mutex);
 		return -1;
+	}
 
 	first_time_init = 0;
-
 
 	page_size = _page_size;
 	mem_size = _mem_size;
@@ -81,6 +218,7 @@ int page_sim_init(unsigned _page_size, unsigned _mem_size,
 
 	// TODO errors?
 	frames = (int *) malloc(2 * mem_size);
+	PaO = (frame*) malloc(mem_size);
 	virtual_memory = (int*) malloc(page_size * mem_size);
 	size_t i;
 	for(i = 0; i < mem_size; i++)
@@ -89,6 +227,7 @@ int page_sim_init(unsigned _page_size, unsigned _mem_size,
 		set_frame(i, ND, -1);
 	}
 	empty_frames = mem_size;
+	pthread_mutex_unlock(&mutex);
 
 	return 0;
 }
@@ -100,22 +239,46 @@ void page_sim_end()
 		printf("page_sim_end() called\n");
 
 	// TODO errno?
-	if(!first_time_end)
+	pthread_mutex_lock(&mutex);
+	if(end)
+	{
+		pthread_mutex_unlock(&mutex);
 		return;
+	}
 
-	first_time_end = 0;
+	end = 1;
 
 	free(frames);
+	free(PaO);
 	free(virtual_memory);
+	pthread_mutex_unlock(&mutex);
 }
 
 
 int page_sim_get(unsigned a, uint8_t *v)
 {
+	pthread_mutex_lock(&mutex);
+	if(end)
+	{
+		pthread_mutex_unlock(&mutex);
+		return -1;
+	}
+	number_of_pagesim_call++;
+	pthread_mutex_unlock(&mutex);
 	return 0;
 }
 
 int page_sim_set(unsigned a, uint8_t v)
 {
+	pthread_mutex_lock(&mutex);
+	if(end)
+	{
+		pthread_mutex_unlock(&mutex);
+		return -1;
+	}
+	number_of_pagesim_call++;
+	int page = a / page_size;
+	int place = a % page_size;
+	pthread_mutex_unlock(&mutex);
 	return 0;
 }
